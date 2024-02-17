50ced4f4718c8b80f292b070c57dcf6c186bc12c
Kraaven <dhruv.bhojaraj@gmail.com>
2024-02-18 02:46:18 +0530

    update 1
    -Created Spline generation

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Splines;
 
 public class Spawner : MonoBehaviour
 {
@@ -10,10 +12,14 @@ public class Spawner : MonoBehaviour
     public bool place;
 
     public List<GameObject> Dots;
+    public Spline line;
+
+    public float threshold;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Dots = new List<GameObject>();
+        line = GetComponent<SplineContainer>()[0];
     }
 
     // Update is called once per frame
@@ -22,14 +28,41 @@ public class Spawner : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             place = !place;
+            if (place)
+            {
+                Dots.Add(Instantiate(Dot, Camera.main.ScreenToWorldPoint(Input.mousePosition)+ new Vector3(0,0,1) ,Quaternion.identity));
+            }
+            else
+            {
+                line.Clear();
+
+                foreach (var point in Dots)
+                {
+                    var knot = new BezierKnot(point.transform.position);
+                    knot.Rotation = point.transform.rotation;
+                    line.Add(knot);
+                }
+                
+                line.SetTangentMode(TangentMode.AutoSmooth);
+                
+            }
         }
+        
     }
 
     public void FixedUpdate()
     {
         if (place)
         {
-            Dots.Add(Instantiate(Dot, Input.mousePosition,Quaternion.identity));
+            Vector3 InputPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (Vector3.Distance(Dots[Dots.Count-1].transform.position,InputPos) > threshold)
+            {
+                Dots.Add(Instantiate(Dot, InputPos+ new Vector3(0,0,1) ,Quaternion.identity));
+                var Diff =Dots[Dots.Count - 1].transform.InverseTransformPoint(Dots[Dots.Count - 2].transform.position);
+                var angle = Mathf.Atan2(Diff.x, Diff.y)*Mathf.Rad2Deg;
+                Dots[Dots.Count - 1].transform.Rotate(0,0, -angle + 180);
+            }
+        
         }
     }
 }
